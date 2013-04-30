a1aeaf3336e0772cdb1129a9b52dc662cc72b74a
feldob <felix.dobslaw@miun.se>
2013-04-30 11:49:27 +0200

    removed loads of class files

--- Java/Source/PerformanceAnalyzerExample/src/miun/se/itm/input/example/performance/Testc.java
+++
@@ -1,9 +0,0 @@
-package miun.se.itm.input.example.performance;
-
-public class Testc {
-
-	public static void main(String[] args) {
-		System.out.println(new long[]{}.getClass().toString());
-	}
-}
-
