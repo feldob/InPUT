7f5a8c05902aec8f4782a93bc7c5ee780a9374e6
feldob <felix.dobslaw@miun.se>
2013-09-11 20:45:52 +0200

    new combined test for all designs.

---
+++ Java/src/Tests/src/se/miun/itm/input/model/design/AllDesignTests.java
@@ -0,0 +1,11 @@
+package se.miun.itm.input.model.design;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DesignSpaceTest.class, DesignTest.class })
+public class AllDesignTests {
+
+}
